0182425a5a7d6f3b71ecdb5d0abd5939cc37c144
万年竹 <122083471+panpan02222@users.noreply.github.com>
2024-02-02 02:01:51 +0000

    图书管理系统,学习代码

---
+++ booktest/book_managerment/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=32)
+    age = models.SmallIntegerField()
+    gender_choices = (
+        (0, "女"),
+        (1, "男"),
+        (2, "保密"),
+    )
+    gender = models.SmallIntegerField(choices=gender_choices)
+    tel = models.CharField(max_length=32)
+    addr = models.CharField(max_length=64)
+    birthday = models.DateField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=32)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    pub_date = models.DateField()
+    publish = models.ForeignKey("Publish", on_delete=models.CASCADE)
+    authors = models.ManyToManyField("Author")
+
+
+class Publish(models.Model):
+    name = models.CharField(max_length=32)
+    city = models.CharField(max_length=64)
+    email = models.EmailField()
